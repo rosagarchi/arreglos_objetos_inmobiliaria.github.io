d29139d1d57c7e8f0071c3d3d16fd62ce74cee68
ROSIITA GARCIA <rosadgarcia91@gmail.com>
2024-12-16 01:11:08 -0300

    arreglo error

--- assets/js/propiedades_alquiler.js
+++ assets/js/propiedades_alquiler.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Renderizar primeras 3 propiedades de venta y alquiler
-  renderPropiedades(propiedadesVenta, "contenedor_propiedades_alquiler");
+  renderPropiedades(propiedadesAlquiler, "contenedor_propiedades_alquiler");
 });
